2b9b45da09bf405bfa9ab905b12c4496a6ba41df
Kosto1221 <ostolany@gamil.com>
2024-11-27 08:56:07 +0900

    is_liked

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,6 +5,7 @@ from categories.serializers import CategorySerializer
 from rest_framework import serializers
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
@@ -26,6 +27,7 @@ class RoomDetailSerializer(ModelSerializer):
     )
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -39,6 +41,10 @@ class RoomDetailSerializer(ModelSerializer):
     def get_is_owner(self, room):
         request = self.context['request']
         return room.owner == request.user
+    
+    def get_is_liked(self, room):
+        request = self.context['request']
+        return Wishlist.objects.filter(user=request.user, rooms__pk=room.pk).exists()
 
 class RoomListSerializer(ModelSerializer):
 
