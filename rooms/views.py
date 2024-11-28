bc683b71efc8033caa8b881240f76ce69ddfd99b
Kosto1221 <ostolany@gamil.com>
2024-11-28 09:40:09 +0900

    Validate Booking

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
+from rest_framework.exceptions import NotFound, ParseError, PermissionDenied
 from .models import Amenity, Room
 from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
@@ -11,9 +11,9 @@ from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 from django.utils import timezone
-import datetime
+
 
 class Amenities(APIView):
     def get(self, request):
@@ -253,6 +253,14 @@ class RoomBookings(APIView):
         bookings = Booking.objects.filter(room=room, kind=Booking.BookingKindChoices.ROOM, check_in__gt=now,)
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
 
 
         # 월/년 별로 보기
