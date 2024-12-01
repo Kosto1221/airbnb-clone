b2d0272413049a1d8c5d150027a2d1c334660604
Kosto1221 <ostolany@gamil.com>
2024-12-01 09:43:38 +0900

    Log In and Log Out

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -75,4 +76,29 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             raise ParseError
+        
+class LogIn(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(request, username=username, password=password,)
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+        
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok":"bye!"})
+
+
+        
 
