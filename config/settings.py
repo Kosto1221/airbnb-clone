7c6e1b106458869a496f8064f1b51cb1e2fde3fc
Kosto1221 <ostolany@gamil.com>
2024-12-23 21:10:25 +0900

    Token Authentication

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
     "strawberry.django"
 ]
 
@@ -155,6 +156,7 @@ PAGE_SIZE = 3
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "config.authentication.TrustMeBroAuthentication"
+        "config.authentication.TrustMeBroAuthentication",
+        "rest_framework.authentication.TokenAuthentication"
     ]
 }
